89f31b46375770ce3b695842498bd98b6394c074
kmsheng <kmsh3ng@gmail.com>
2018-03-09 17:39:40 +0800

    add example of using relational conditional

---
+++ src/relational-conditional.c
@@ -0,0 +1,14 @@
+// https://openhome.cc/Gossip/CGossip/RelationalConditional.html
+#include <stdio.h>
+
+int main(void) {
+
+    printf("10 > 5\t\t%d\n", 10 > 5);
+    printf("10 >= 5\t\t%d\n", 10 >= 5);
+    printf("10 < 5\t\t%d\n", 10 < 5);
+    printf("10 <= 5\t\t%d\n", 10 <= 5);
+    printf("10 == 5\t\t%d\n", 10 == 5);
+    printf("10 != 5\t\t%d\n", 10 != 5);
+
+    return 0;
+}
