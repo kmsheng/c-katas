c3993194987befdb322a08b717d23975767ce51d
kmsheng <kmsh3ng@gmail.com>
2018-03-09 15:47:51 +0800

    add printf example

---
+++ src/printf.c
@@ -0,0 +1,9 @@
+// https://openhome.cc/Gossip/CGossip/PrintfScanf.html
+#include <stdio.h>
+
+int main(void) {
+    int count = printf("This is a test!\n");
+    printf("%d\n", count);
+
+    return 0;
+}
