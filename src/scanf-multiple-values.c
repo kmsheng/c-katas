c2ae8c5c0a515bec49a86b13223c73127ccd30f4
kmsheng <kmsh3ng@gmail.com>
2018-03-09 16:35:02 +0800

    add example of getting multiple values by scanf function

---
+++ src/scanf-multiple-values.c
@@ -0,0 +1,17 @@
+// https://openhome.cc/Gossip/CGossip/PrintfScanf.html
+#include <stdio.h>
+
+int main(void) {
+
+    int num1, num2;
+
+    printf("Please input two numbers, separate them with space: ");
+    scanf("%d %d", &num1, &num2);
+    printf("The numbers that you input: %d %d\n", num1, num2);
+
+    printf("Please input two numbers again");
+    scanf("%d %d", &num1, &num2);
+    printf("Here you go: %d %d", num1, num2);
+
+    return 0;
+}
