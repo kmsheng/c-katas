ab3f1e75e7a358fc553510d93563d3425f476df7
kmsheng <kmsh3ng@gmail.com>
2018-03-09 16:29:56 +0800

    add scanf example

---
+++ src/scanf.c
@@ -0,0 +1,14 @@
+// https://openhome.cc/Gossip/CGossip/PrintfScanf.html
+#include <stdio.h>
+
+int main(void) {
+
+    int input;
+
+    printf("Please input number:");
+    scanf("%d", &input);
+
+    printf("The number that you input: %d\n", input);
+
+    return 0;
+}
