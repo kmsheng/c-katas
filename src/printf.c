0c419b745a28c714ddbd438590bb17c48b05b3df
kmsheng <kmsh3ng@gmail.com>
2018-03-09 16:17:19 +0800

    add example of how to limit decimal places

--- src/printf.c
+++ src/printf.c
@@ -5,5 +5,8 @@ int main(void) {
     int count = printf("This is a test!\n");
     printf("%d\n", count);
 
+    // limit decimal places
+    printf("example: %.2f\n", 19.2345678);
+
     return 0;
 }
