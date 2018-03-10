c57903b43f5917e66b5a3a4a797361a746c986db
kmsheng <kmsh3ng@gmail.com>
2018-03-10 13:54:56 +0800

    add example of how to define fixed values two dimension array in C

---
+++ src/two-dim-arr2.c
@@ -0,0 +1,14 @@
+// https://openhome.cc/Gossip/CGossip/TwoDimArray.html
+#include <stdio.h>
+
+int main(void) {
+
+    // this is how to define fixed values of two dimension array in C
+    int maze[3][3] = {
+        {0, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0}
+    };
+
+    return 0;
+}
