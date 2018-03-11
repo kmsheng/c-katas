5f707ed4145d9b0f1c753e97fd3330943db4d7a9
kmsheng <kmsh3ng@gmail.com>
2018-03-11 23:08:08 +0800

    add pointer example

---
+++ pointer.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    int num = 10;
+
+    printf("variable num's value: %d\n", num);
+    printf("variable num's memory address: %p\n", &num);
+
+    return 0;
+}
